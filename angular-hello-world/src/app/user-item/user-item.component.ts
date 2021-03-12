d55d05ef00d69134bc2c04a50acbb39b96a92c71
Katrin Yordanova <katrinyordanova@abv.bg>
2021-03-12 19:11:53 +0200

    added user-list component

---
+++ angular-hello-world/src/app/user-item/user-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.scss']
+})
+export class UserItemComponent implements OnInit {
+  @Input() name: string;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
