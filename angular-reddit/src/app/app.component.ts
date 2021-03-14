01d531a7b2c25cdfe1b20eb92adb2b7282e3cce0
Katrin Yordanova <katrinyordanova@abv.bg>
2021-03-14 17:34:01 +0200

    added link form and its styles to app component

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-reddit';
+
+  form = new FormGroup({
+    title: new FormControl(''),
+    link: new FormControl('')
+  });
+  
+  onSubmit() {
+
+  }
 }
