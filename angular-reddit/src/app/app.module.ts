20e1543f0777107823895351bca3aecf1ebba47a
Katrin Yordanova <katrinyordanova@abv.bg>
2021-03-15 19:31:57 +0200

    removed styles in styles.scss

--- angular-reddit/src/app/app.module.ts
+++ angular-reddit/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ArticlesComponent } from './articles/articles.component';
 import { ArticleComponent } from './articles/article/article.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
