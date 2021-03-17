3b5b0634894faa51a9f72b05ea70d2fc8f814115
Katrin Yordanova <katrinyordanova@abv.bg>
2021-03-17 19:27:25 +0200

    changed app-root to inventory-app-root

--- how-angular-works/src/app/app.component.ts
+++ how-angular-works/src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'inventory-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'how-angular-works';
+  products: Product[];
+
+  constructor() {
+    let newProduct = [
+    new Product('NICEHAT',
+      'A Nice Black Hat',
+      '/assets/images/products/black-hat.jpg',
+      ['Men', 'Accessories', 'Hats'], 
+      29.99),
+    new Product(
+      'NEATOJACKET', 
+      'Blue Jacket',
+      '/assets/images/products/blue-jacket.jpg', 
+      ['Women', 'Apparel', 'Jackets & Vests'], 
+      238.99),
+    new Product(
+      'NICEHAT',
+      'A Nice Black Hat',
+      '/assets/images/products/black-hat.jpg', 
+      ['Men', 'Accessories', 'Hats'], 
+      29.99)
+    ];
+    
+    this.products = newProduct;
+  }
+
+  productWasClicked(product: Product): void {
+    console.log(`Product clicked: ${product}`);
+  }
 }
