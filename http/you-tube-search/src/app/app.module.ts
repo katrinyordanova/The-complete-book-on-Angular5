388761fcf41cc0dba9b4a956fcd4ba042e221674
Katrin Yordanova <katrinyordanova@abv.bg>
2021-03-26 18:50:09 +0200

    added you-tube-serach component

--- http/you-tube-search/src/app/app.module.ts
+++ http/you-tube-search/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { youtubeSearchInjectables } from './you-tube-search.injectables';
+import { SearchBoxComponent } from './search-box/search-box.component';
+import { SearchResultComponent } from './search-result/search-result.component';
+import { YouTubeSearchComponent } from './you-tube-search/you-tube-search.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBoxComponent,
+    SearchResultComponent,
+    YouTubeSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ youtubeSearchInjectables ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
