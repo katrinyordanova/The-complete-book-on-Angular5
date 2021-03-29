c0ca62ae0d1c61994b7b73579882c967a6cac574
Katrin Yordanova <katrinyordanova@abv.bg>
2021-03-29 19:37:54 +0300

    created product and products

---
+++ routes/routing/src/app/product/product.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+  id: string;
+
+  constructor(private route: ActivatedRoute) {
+    route.params.subscribe((params) => { this.id = params['id']; });
+  }
+
+  ngOnInit() {
+  }
+
+}
