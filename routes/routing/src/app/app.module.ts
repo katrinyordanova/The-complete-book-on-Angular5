be8455affd37c01d975afac508520b12edc3090e
Katrin Yordanova <katrinyordanova@abv.bg>
2021-03-29 19:39:16 +0300

    added routes and gif for loading component

--- routes/routing/src/app/app.module.ts
+++ routes/routing/src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { ProtectedComponent } from './protected/protected.component';
+import { ProductsComponent } from './products/products.component';
+import { HeaderComponent } from './header/header.component';
+import { routes } from './routes';
+import { LoadingComponent } from './loading/loading.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent,
+    ProtectedComponent,
+    ProductsComponent,
+    HeaderComponent,
+    LoadingComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ routes/routing/src/app/routes.ts
@@ -0,0 +1,31 @@
+import { Routes } from '@angular/router';
+
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
+import { ProductsComponent } from './products/products.component';
+import { ProtectedComponent } from './protected/protected.component';
+
+export const routes: Routes = [
+    // basic routes
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'contactus', redirectTo: 'contact' },
+    // authentication routes
+    { path: 'login', component: LoginComponent },
+    {
+      path: 'protected',
+      component: ProtectedComponent,
+    //   canActivate: [ LoggedInGuard ]
+    },
+    // nested
+    {
+      path: 'products',
+      component: ProductsComponent,
+    //   children: childRoutes
+    },
+    { path: 'product/:id', component: ProductComponent }
+  ]
