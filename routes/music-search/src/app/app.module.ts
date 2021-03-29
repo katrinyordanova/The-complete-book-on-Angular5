1a88467cd36d30712b4bc5d70e2d72e8bcefdbbf
Katrin Yordanova <katrinyordanova@abv.bg>
2021-03-29 20:48:51 +0300

    created loading, artists, albums, tracks and search components

--- routes/music-search/src/app/app.module.ts
+++ routes/music-search/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { TracksComponent } from './tracks/tracks.component';
+import { LoadingComponent } from './loading/loading.component';
+import { routes } from './routes';
+import { HttpModule } from '@angular/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ArtistsComponent,
+    AlbumsComponent,
+    TracksComponent,
+    LoadingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
