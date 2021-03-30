990324bc667e373458ce2dfc3708574d6188b697
Katrin Yordanova <katrinyordanova@abv.bg>
2021-03-30 15:31:08 +0300

    added template and search, submit, renderResults functionality to search component

--- routes/music-search/src/app/search/search.component.ts
+++ routes/music-search/src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SpotifyService } from '../spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  query: string;
+  results: Object;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private spotifyService: SpotifyService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) 
+  {
+    this.route.queryParams.subscribe(params => { this.query = params['query'] || ''; });
   }
 
+  ngOnInit(): void {
+    this.search();
+  }
+
+  search(): void {
+    console.log('this.query: ', this.query);
+    if(!this.query) {
+      return;
+    }
+
+    this.spotifyService.searchTrack(this.query).subscribe((res: any) => this.renderResults(res));
+  }
+
+  renderResults(res: any): void {
+    this.results = null;
+    if(res && res.tracks && res.tracks.items) {
+      this.results = res.tracks.items;
+    }
+  }
+
+  submit(query: string): void {
+    this.router.navigate(['search'], { queryParams: { query: query } })
+      .then(_ => this.search());
+  }
 }
