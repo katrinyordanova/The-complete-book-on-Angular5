c184139823c73b1f3061799de5a73f100271240f
Katrin Yordanova <katrinyordanova@abv.bg>
2021-03-30 15:32:43 +0300

    added template and back, renderTrack funtionality to tracks component

--- routes/music-search/src/app/tracks/tracks.component.ts
+++ routes/music-search/src/app/tracks/tracks.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { SpotifyService } from '../spotify.service';
 
 @Component({
   selector: 'app-tracks',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tracks.component.scss']
 })
 export class TracksComponent implements OnInit {
+  id: string;
+  track: Object;
 
-  constructor() { }
+  constructor(
+    private spotifyService: SpotifyService,
+    private location: Location
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.spotifyService
+      .getTrack(this.id)
+      .subscribe((res: any) => this.renderTrack(res));
   }
 
+  renderTrack(res: any): void {
+    this.track = res;
+  }
+  back(res: any) {
+    this.location.back();
+  }
 }
