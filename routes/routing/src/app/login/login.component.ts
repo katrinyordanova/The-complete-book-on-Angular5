6e67cf74a4c2dd8d8b79ea14850bdafeb015a7fd
Katrin Yordanova <katrinyordanova@abv.bg>
2021-03-30 18:59:48 +0300

    added logic to login and protected component

--- routes/routing/src/app/login/login.component.ts
+++ routes/routing/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  message: string;
+  
+  constructor(public authService: AuthService) {
+    this.message = '';
+  }
 
   ngOnInit() {
   }
 
+  login(username: string, password: string): boolean {
+    this.message = '';
+    if(!this.authService.login(username, password)) {
+      this.message = 'Incorrect credentials.';
+      setTimeout(() => {
+        this.message = ''
+      },(2500));
+    }
+    return false;
+  }
+
+  logout(): boolean {
+    this.authService.logout();
+    return false;
+  }
 }
