a521a0f32fd51d1c6be93381f70d259d3d38ce70
Katrin Yordanova <katrinyordanova@abv.bg>
2021-04-02 18:34:54 +0300

    created thread model

---
+++ rxjs/rxjs-chat/src/app/thread/thread.model.ts
@@ -0,0 +1,22 @@
+import { Message } from '../message/message.model';
+import { uuid } from '../../util/uuid';
+
+/**
+ * Thread represents a group of Users exchanging Messages
+*/
+export class Thread {
+    id: string;
+    lastMessage: string;
+    name: string;
+    avatarSrc: string;
+
+    constructor(
+        id?: string,
+        name?: string,
+        avatarSrc?: string
+    ) {
+        this.id = id || uuid();
+        this.name = name;
+        this.avatarSrc = avatarSrc;
+    }
+}
