e70dc61a1c9ed127c5e0d94dfcb86074bb9f209a
Katrin Yordanova <katrinyordanova@abv.bg>
2021-04-04 19:39:38 +0300

    added chat-page to app component template, provided dependencies and added new components to app module

--- rxjs/rxjs-chat/src/app/app.component.ts
+++ rxjs/rxjs-chat/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import { MessageService } from './message/message.service';
+import { ThreadService } from './thread/thread.service';
+import { UserService } from './user/user.service';
+import { ChatExampleData } from './data/chat-example-data';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(
+    public messageService: MessageService,
+    public threadsService: ThreadService,
+    public userService: UserService
+  ) 
+  {
+    ChatExampleData.init(messageService, threadsService, userService)  
+  }
 }
--- rxjs/rxjs-chat/src/app/app.module.ts
+++ rxjs/rxjs-chat/src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ChatThreadsComponent } from './chat-threads/chat-threads.component';
+import { ChatThreadComponent } from './chat-thread/chat-thread.component';
+import { ChatPageComponent } from './chat-page/chat-page.component';
+import { MessageService } from './message/message.service';
+import { ThreadService } from './thread/thread.service';
+import { UserService } from './user/user.service';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
+import { ChatMessageComponent } from './chat-message/chat-message.component';
+import { FormsModule } from '@angular/forms';
+import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatThreadsComponent,
+    ChatThreadComponent,
+    ChatPageComponent,
+    ChatWindowComponent,
+    ChatMessageComponent,
+    ChatNavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [MessageService, ThreadService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
