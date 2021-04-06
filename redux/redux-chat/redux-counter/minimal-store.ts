4b380e53001d08aa9c86fb11ae38d377ed3f08f4
Katrin Yordanova <katrinyordanova@abv.bg>
2021-04-06 16:57:37 +0300

    added simple store

---
+++ redux/redux-chat/redux-counter/minimal-store.ts
@@ -0,0 +1,28 @@
+import { Action, Reducer } from "./identity-reducer";
+import { reducer } from "./plus-action";
+
+class Store<T> {
+    private _state: T;
+
+    constructor(
+        private reducer: Reducer<T>,
+        initialState: T
+    ) {
+        this._state = initialState;
+    }
+
+    getState(): T {
+        return this._state;
+    }
+
+    dispatch(action: Action) {
+        this._state = this.reducer(this._state, action);
+    }
+}
+
+let store = new Store<number>(reducer, 0);
+console.log(store.getState());
+
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'DECREMENT' });
+store.dispatch({ type: 'PLUS' });
