dc1392195eda0931c040ef32b6c8b71af582b537
Katrin Yordanova <katrinyordanova@abv.bg>
2021-04-06 19:32:37 +0300

    integrated store with redux devtools and created app store

---
+++ redux/redux-chat/redux-counter/src/app.store.ts
@@ -0,0 +1,23 @@
+import { InjectionToken } from "@angular/core";
+import { compose, createStore, Store, StoreEnhancer } from "redux";
+import { AppState } from "./app.state";
+import {
+    counterReducer as reducer
+} from './counter.reducer';
+
+export const AppStore = new InjectionToken('App.store');
+
+const devtools: StoreEnhancer<AppState> =
+    window['devToolsExtension'] ? 
+    window['devToolsExtension']() : f => f;
+
+export function createAppStore(): Store<AppState> {
+    return createStore<AppState, any, any, any> (
+        reducer,
+        compose(devtools)
+    )
+}
+
+export const appStoreProviders = [
+    { provide: AppStore, useFactory: createAppStore }
+]
