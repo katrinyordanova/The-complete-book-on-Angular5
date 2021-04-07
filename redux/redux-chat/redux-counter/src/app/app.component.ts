b5e28d51946a91fea3bd482b4fa4506b902d2287
Katrin Yordanova <katrinyordanova@abv.bg>
2021-04-07 14:19:56 +0300

    added counter to app model and template

--- redux/redux-chat/redux-counter/src/app/app.component.ts
+++ redux/redux-chat/redux-counter/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Store } from 'redux';
+import { AppStore } from './app.store';
+import { AppState } from './app.state';
+import * as CounterActions from './counter.actions';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  counter: number;
+
+  constructor(@Inject(AppStore) private store: Store<AppState>) {
+    store.subscribe(() => this.readState());
+    this.readState();
+  }
+
+  redState() {
+    const state: AppState = this.store.getState() as AppState;
+    this.counter = state.counter;
+  }
+
+  increment() {
+    this.store.dispatch(CounterActions.increment());
+  }
+
+  decrement() {
+    this.store.dispatch(CounterActions.decrement());
+  }
 }
