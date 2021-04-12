f1b2515b4d75000f10058f8128c3c9371083c69c
Katrin Yordanova <katrinyordanova@abv.bg>
2021-04-12 17:14:10 +0300

    added chat-message, chat-thread, chat-threads and chat-window components

--- redux/redux-chat/src/app/app.module.ts
+++ redux/redux-chat/src/app/app.module.ts
@@ -5,13 +5,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ChatPageComponent } from './chat-page/chat-page.component';
 import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
+import { ChatThreadsComponent } from './src/app/chat-threads/chat-threads.component';
+import { ChatThreadComponent } from './chat-thread/chat-thread.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
+import { ChatMessageComponent } from './chat-message/chat-message.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatPageComponent,
-    ChatNavBarComponent
+    ChatNavBarComponent,
+    ChatThreadsComponent,
+    ChatThreadComponent,
+    ChatWindowComponent,
+    ChatMessageComponent
   ],
   imports: [
     BrowserModule
---
+++ redux/redux-chat/src/app/chat-threads/chat-threads.component.ts
@@ -0,0 +1,40 @@
+import {
+  Component,
+  Inject
+} from '@angular/core';
+import { AppStore } from '../app.store';
+import * as Redux from 'redux';
+import {
+  Thread
+} from '../thread/thread';
+import * as ThreadActions from '../thread/thread.actions';
+import {
+  AppState,
+  getCurrentThread,
+  getAllThreads
+} from '../app.reducer';
+
+@Component({
+  selector: 'chat-threads',
+  templateUrl: './chat-threads.component.html',
+  styleUrls: ['./chat-threads.component.css']
+})
+export class ChatThreadsComponent {
+  threads: Thread[];
+  currentThreadId: string;
+
+  constructor(@Inject(AppStore) private store: Redux.Store<AppState>) {
+    store.subscribe(() => this.updateState());
+    this.updateState();
+  }
+
+  updateState() {
+    const state = this.store.getState();
+    this.threads = getAllThreads(state);
+    this.currentThreadId = getCurrentThread(state).id;
+  }
+
+  handleThreadClicked(thread: Thread) {
+    this.store.dispatch(ThreadActions.selectThread(thread));
+  }
+}
