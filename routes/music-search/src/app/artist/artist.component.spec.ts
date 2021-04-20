dd90968d395deaedaf20baeb166b6cc58fb54d33
Katrin Yordanova <katrinyordanova@abv.bg>
2021-04-20 20:41:19 +0300

    added test which checks if the artist component is initialized properly

--- routes/music-search/src/app/artist/artist.component.spec.ts
+++ routes/music-search/src/app/artist/artist.component.spec.ts
@@ -1,7 +1,25 @@
-import { configureMusicTests } from "../test/test.module";
+import { inject } from "@angular/core/src/render3";
+import { fakeAsync } from "@angular/core/testing";
+import { Router } from "@angular/router";
+
+import { SpotifyService } from "../spotify.service";
+import { MockSpotifyService } from "../test/spotify.service.mock";
+import { advance, configureMusicTests, createRoot, RootCmp } from "../test/test.module";
 
 describe('ArtistComponent', () => {
   beforeEach(async() => {
     configureMusicTests();
   });
+
+  describe('initialization', () => {
+    it('retrieves the artists', fakeAsync(
+      inject([Router, SpotifyService],
+        (router: Router, mockSpotify: MockSpotifyService) => {
+          const fixture = createRoot(router, RootCmp);
+          router.navigateByUrl('/artists/2');
+          advance(fixture);
+          expect(MockSpotifyService.getArtistSpy).toHaveBeenCalledWith('2');
+        })
+    ) )
+  })
 })
