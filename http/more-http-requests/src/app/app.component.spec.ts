d37aafde6d4ddeeda76677e9af1458572f7351c5
Katrin Yordanova <katrinyordanova@abv.bg>
2021-04-21 15:04:29 +0300

    added unit tests which check if POST and DELETE requests are correctly sent and  headers as well

--- http/more-http-requests/src/app/app.component.spec.ts
+++ http/more-http-requests/src/app/app.component.spec.ts
@@ -1,27 +1,64 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { async, ComponentFixture, inject, TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let httpMock: HttpTestingController;
+
+  beforeEach(async() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+      imports: [HttpClientTestingModule],
+      declarations: [AppComponent]
+    });
+  });
+
+  beforeEach(async(
+    inject([HttpTestingController], _httpMock => {
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+      httpMock = _httpMock;
+    }))
+  )
+
+  afterEach(
+    inject([HttpTestingController], (httpMock: HttpTestingController) => {
+      httpMock.verify();
+    })
+  );
+
+  it('should send a POST request', async(() => {
+    component.makePost();
+    const req = httpMock.expectOne(
+      'http://jsonplaceholder.typicode.com/posts'
+    );
+    expect(req.request.method).toEqual('POST');
+    req.flush({ response: 'OK' });
+    expect(component.data).toEqual({ response: 'OK' });
+    httpMock.verify();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+
+  it('should send a DELETE request', async(() => {
+    component.makeDelete();
+    const req = httpMock.expectOne(
+      'http://jsonplaceholder.typicode.com/posts/1'
+    );
+    expect(req.request.method).toEqual('DELETE');
+    req.flush({ response: 'OK' });
+    expect(component.data).toEqual({ response: 'OK' });
+    httpMock.verify();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+
+  it('should send correct headers', async(() => {
+    component.makeHeaders();
+    const req = httpMock.expectOne(req =>
+      req.headers.has('X-API-TOKEN') &&
+      req.headers.get('X-API-TOKEN') === 'ng-book'
+    );
+    req.flush({ response: 'OK' });
+    expect(component.data).toEqual({ response: 'OK' });
+    httpMock.verify();
   }));
 });
